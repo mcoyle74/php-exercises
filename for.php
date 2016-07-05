876b0f19fcf281da5ba9ef07f22d5a0cf3ce9f4c
Michael Coyle <mcoyle74@gmail.com>
2016-07-05 12:13:39 -0500

    Added nested if statements that check for a null value on the increment and if it is numeric. Null defaults to an increment of 1. Non-numeric fwrites a STDERR message.

--- for.php
+++ for.php
@@ -7,6 +7,17 @@ $endNumber =  trim(fgets(STDIN));
 fwrite(STDOUT, 'Choose an increment: ');
 $increment = trim(fgets(STDIN));
 
-for ($i = $startNumber; $i <= $endNumber; $i += $increment) {
-	echo "{$i}\n";
+if ($increment != NULL) {
+	if (is_numeric($increment)) {
+		for ($i = $startNumber; $i <= $endNumber; $i += $increment) {
+		echo "{$i}\n";
+		}
+	} else {
+		fwrite(STDERR, "The increment must be numeric.\n");
+		exit(1);
+	}
+} else {
+	for ($i = $startNumber; $i <= $endNumber; $i++) {
+		echo "{$i}\n";
+		}
 }
