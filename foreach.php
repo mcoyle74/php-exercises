f018a831a6ab4aac8331d6970999b9df7d60031f
Michael Coyle <mcoyle74@gmail.com>
2016-07-05 15:25:11 -0500

    Coded a foreach loop that echos each value, including nested arrays.

--- foreach.php
+++ foreach.php
@@ -4,22 +4,34 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 foreach ($things as $thing) {
 	if (is_int($thing)) {
-		echo "{$thing} is an integer.\n";
+		echo "{$thing}\n";
 	} elseif (is_float($thing)) {
-		echo "{$thing} is a float.\n";						
+		echo "{$thing}\n";						
 	}
 	 elseif (is_bool($thing)) {
-		echo "This is a boolean: ";
-		var_dump($thing);
+		echo "{$thing}\n";
 	}
 	 elseif (is_array($thing)) {
-		echo "This is an array: ";
-		print_r($thing);
+	 	echo 'Array (';
+	 	foreach ($thing as $value) {
+			echo "{$value}";
+	 	}
+	 	echo ')' . PHP_EOL;
 	}
 	 elseif (is_null($thing)) {
-		echo "This value is NULL.\n";
+		echo "{$thing}\n";
 	}
 	 elseif (is_string($thing)) {
-		echo "{$thing} is a string.\n";
+		echo "{$thing}\n";
 	}
 }
+
+// foreach ($things as $thing) {
+// 	if (is_scalar($thing)) {
+// 		if (is_bool($thing)) {
+// 			var_dump($thing);
+// 		} else {
+// 			echo $thing . PHP_EOL;
+// 		}
+// 	}
+// }
