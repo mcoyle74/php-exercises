dd9c73dd7b81bba6d8533c6ff4e60e0afdd61dfc
Michael Coyle <mcoyle74@gmail.com>
2016-07-05 12:02:41 -0500

    Completed step 2 of exercise, asking user for an increment. added gitignore file.

--- for.php
+++ for.php
@@ -4,7 +4,9 @@ fwrite(STDOUT, 'Choose a starting number: ');
 $startNumber = trim(fgets(STDIN));
 fwrite(STDOUT, 'Choose an ending number: ');
 $endNumber =  trim(fgets(STDIN));
+fwrite(STDOUT, 'Choose an increment: ');
+$increment = trim(fgets(STDIN));
 
-for ($i = $startNumber; $i <= $endNumber; $i++) {
+for ($i = $startNumber; $i <= $endNumber; $i += $increment) {
 	echo "{$i}\n";
 }
