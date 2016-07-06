8dc8abf9d60d67930ada69868a953dbba9c0d1f0
Michael Coyle <mcoyle74@gmail.com>
2016-07-06 10:03:03 -0500

    Created another loop that counts from 1 to 100 but stops after 10 because of a break.

--- break_continue.php
+++ break_continue.php
@@ -1,9 +1,17 @@
 <?php
 
+// foreach (range(1, 100) as $i) {
+// 	if ($i % 2 == 0) {
+// 		echo "{$i}\n";
+// 	} else {
+// 		continue;
+// 	}
+// }
+
 foreach (range(1, 100) as $i) {
-	if ($i % 2 == 0) {
+	if ($i <= 10) {
 		echo "{$i}\n";
 	} else {
-		continue;
+		break;
 	}
 }
