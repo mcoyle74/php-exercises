99b7923d99ad2b8ac5f021a47758aa3c27be86dd
Michael Coyle <mcoyle74@gmail.com>
2016-07-06 14:54:50 -0500

    Trimmed confirm and prompt functions of unnecessary code. Fixed do/while to keep asking as long as the answer to 'add another grade' is 'yes.'

--- grades.php
+++ grades.php
@@ -1,29 +1,23 @@
 <?php
 
 function prompt($prompt) {
-    fwrite(STDOUT, $prompt);
-    return STDIN;
+	fwrite(STDOUT, $prompt);
 }
 
 function confirm($confirm) {
-    fwrite(STDOUT, $confirm . "yes/no \n");
-    if (STDIN == trim('yes')) {
-        return true;
-    } elseif (STDIN == trim('no')) {
-        return false;
-    } else {
-        fwrite(STDOUT, $confirm . "yes/no \n");
-    }
+	fwrite(STDOUT, $confirm . "(yes/no) \n");
 }
 
 prompt('What is your name? ');
 $studentName = fgets(STDIN);
 
 do {
-    prompt('What is the name of the subject? ');
-    $name = fgets(STDIN);
-    prompt('What is the grade? ');
-    $grade = fgets(STDIN);
-} while (confirm('Do you want to add another grade? '));
+	prompt('What is the name of the subject? ');
+	$name = fgets(STDIN);
+	prompt('What is the grade? ');
+	$grade = fgets(STDIN);
+	confirm('Do you want to add another grade? ');
+	$confirmed = trim(fgets(STDIN));
+} while ($confirmed == 'yes');
 
-$average = fgets(STDIN);
+echo "done\n";
