8170521d5360016f3308f5df149186014ba150cc
Michael Coyle <mcoyle74@gmail.com>
2016-07-06 10:44:53 -0500

    Added if statement that gives the same output as the switch.

--- switch.php
+++ switch.php
@@ -5,6 +5,23 @@ date_default_timezone_set('America/Chicago');
 // Gets day of week as number: 1 for Monday through 7 for Sunday
 $dayOfWeek = date('N');
 
+if ($dayOfWeek == 1) {
+	echo "Monday\n";
+} elseif ($dayOfWeek == 2) {
+	echo "Tuesday\n";
+} elseif ($dayOfWeek == 3) {
+	echo "Wednesday\n";
+} elseif ($dayOfWeek == 4) {
+	echo "Thursday\n";
+} elseif ($dayOfWeek == 5) {
+	echo "Friday\n";
+} elseif ($dayOfWeek == 6) {
+	echo "Saturday\n";
+} elseif ($dayOfWeek == 7) {
+	echo "Sunday\n";
+}
+
+// Switch does the same thing as if statement above
 switch ($dayOfWeek) {
 	case 1:
 		echo "Monday\n";
