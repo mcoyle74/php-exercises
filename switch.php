4890da2539230330a9e491044587b37dd30635e8
Michael Coyle <mcoyle74@gmail.com>
2016-07-06 10:28:40 -0500

    Created new file for Exercise 6.3.5, Switch.

---
+++ switch.php
@@ -0,0 +1,14 @@
+<?php
+
+date_default_timezone_set('America/Chicago');
+
+// Gets day of week as number: 1 for Monday through 7 for Sunday
+$dayOfWeek = date('N');
+
+switch($dayOfWeek) {
+	case 1:
+		// Output Monday
+	case 2:
+		//Output Tuesday
+	// etc through day 7
+}
