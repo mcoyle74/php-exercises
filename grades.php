b8448bc8fe96fc3f2c893e9c205232fa225b34df
Michael Coyle <mcoyle74@gmail.com>
2016-07-06 16:14:04 -0500

    Created arrays that take in student names, subjects and grades. Wrote an average function.

--- grades.php
+++ grades.php
@@ -1,5 +1,9 @@
 <?php
 
+$students = [];
+$grades = [];
+
+
 function prompt($prompt) {
 	fwrite(STDOUT, $prompt);
 }
@@ -8,16 +12,23 @@ function confirm($confirm) {
 	fwrite(STDOUT, $confirm . "(yes/no) \n");
 }
 
-prompt('What is your name? ');
+function average($sum, $numberOfGrades) {
+	return $sum / $numberOfGrades;
+}
+
+prompt('Student name? ');
 $studentName = fgets(STDIN);
+$students[] = $studentName;
 
 do {
 	prompt('What is the name of the subject? ');
-	$name = fgets(STDIN);
+	$name = fgets(STDIN);  
 	prompt('What is the grade? ');
 	$grade = fgets(STDIN);
+	$grades[] = [$name => $grade];
 	confirm('Do you want to add another grade? ');
 	$confirmed = trim(fgets(STDIN));
 } while ($confirmed == 'yes');
 
-echo "done\n";
+var_dump($students);
+var_dump($grades);
