13c7d6d3f70ab4141a99a134352d56ffd9ec62cd
Michael Coyle <mcoyle74@gmail.com>
2016-07-07 11:26:24 -0500

    Added error message for division by zero. Refactored function if statements to check first for !is_numeric() and changed error message to an fwrite(STDERR) with exit(1).

--- arithmetic.php
+++ arithmetic.php
@@ -1,46 +1,49 @@
 <?php
 
-$a = true;
-$b = 'thoughts';
+$a = false;
+$b = 'existence';
 
 function add($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-	    return $a + $b;
-	} else {
-		return "ERROR: Both arguments must be numbers.\n";
+	if (!is_numeric($a) || !is_numeric($b)) {
+		fwrite(STDERR, "ERROR: arguments must be numbers.\n");
+		exit(1);
 	}
+    return $a + $b;
 }
 
 function subtract($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-	    return $a - $b;
-	} else {
-		return "ERROR: Both arguments must be numbers.\n";
+	if (!is_numeric($a) || !is_numeric($b)) {
+		fwrite(STDERR, "ERROR: arguments must be numbers.\n");
+		exit(1);
 	}
+    return $a - $b;
 }
 
 function multiply($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-	    return $a * $b;
-	} else {
-		return "ERROR: Both arguments must be numbers.\n";
+	if (!is_numeric($a) || !is_numeric($b)) {
+		fwrite(STDERR, "ERROR: arguments must be numbers.\n");
+		exit(1);
 	}
+    return $a * $b;
 }
 
 function divide($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-	    return $a / $b;
-	} else {
-		return "ERROR: Both arguments must be numbers.\n";
+	if (!is_numeric($a) || !is_numeric($b)) {
+		fwrite(STDERR, "ERROR: arguments must be numbers.\n");
+		exit(1);
+	} elseif ($b == 0) {
+		fwrite(STDERR, "ERROR: division by zero.");
+		exit(1);
 	}
+    return $a / $b;
 }
 
 function modulus($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-		return $a % $b;
-	} else {
-		return "ERROR: Both arguments must be numbers.\n";
+	if (!is_numeric($a) || !is_numeric($b)) {
+		fwrite(STDERR, "ERROR: arguments must be numbers.\n");
+		exit(1);
 	}
+	return $a % $b;
 }
 
 echo "{$a} + {$b} = " . add($a, $b), PHP_EOL;
