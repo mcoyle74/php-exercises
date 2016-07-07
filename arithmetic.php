d2cf8355ee4482b6ee92925b4bf519bd8a86272f
Michael Coyle <mcoyle74@gmail.com>
2016-07-07 11:49:00 -0500

    Tasked all error messages to throwErroMessage() function.

--- arithmetic.php
+++ arithmetic.php
@@ -1,19 +1,17 @@
 <?php
 
-$a = false;
-$b = 'existence';
+$a = mt_rand(0, 15);
+$b = mt_rand(0, 15);
 
 function add($a, $b) {
 	if (!is_numeric($a) || !is_numeric($b)) {
-		fwrite(STDERR, "ERROR: arguments must be numbers.\n");
-		exit(1);
+		throwErrorMessage($a, $b, '!n');
 	}
     return $a + $b;
 }
 
 function subtract($a, $b) {
 	if (!is_numeric($a) || !is_numeric($b)) {
-		fwrite(STDERR, "ERROR: arguments must be numbers.\n");
 		exit(1);
 	}
     return $a - $b;
@@ -21,29 +19,35 @@ function subtract($a, $b) {
 
 function multiply($a, $b) {
 	if (!is_numeric($a) || !is_numeric($b)) {
-		fwrite(STDERR, "ERROR: arguments must be numbers.\n");
-		exit(1);
+		throwErrorMessage($a, $b, '!n');
 	}
     return $a * $b;
 }
 
 function divide($a, $b) {
 	if (!is_numeric($a) || !is_numeric($b)) {
-		fwrite(STDERR, "ERROR: arguments must be numbers.\n");
-		exit(1);
+		throwErrorMessage($a, $b, '!n');
 	} elseif ($b == 0) {
-		fwrite(STDERR, "ERROR: division by zero.");
-		exit(1);
+		throwErrorMessage($a, $b, '/0');
 	}
     return $a / $b;
 }
 
 function modulus($a, $b) {
 	if (!is_numeric($a) || !is_numeric($b)) {
-		fwrite(STDERR, "ERROR: arguments must be numbers.\n");
-		exit(1);
+		throwErrorMessage($a, $b, '!n');
 	}
 	return $a % $b;
+}
+
+function throwErrorMessage($a, $b, $error) {
+	if ($error == '!n') {
+		fwrite(STDERR, "ERROR: \$a = {$a} and \$b = {$b}, and both arguments must be numbers.\n");
+		exit(1);
+	} elseif ($error == '/0') {
+		fwrite(STDERR, "ERROR: division by zero.\n");
+		exit(1);
+	}
 }
 
 echo "{$a} + {$b} = " . add($a, $b), PHP_EOL;
