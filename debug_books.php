3c1c3935f45aa95764efeff0762662f5c969bde7
Michael Coyle <mcoyle74@gmail.com>
2016-07-08 10:48:11 -0500

    Created new file for debugging exercise.

---
+++ debug_books.php
@@ -0,0 +1,59 @@
+<?php
+
+$friends = [
+    [
+        'name' => 'Oliver',
+        'books' => [
+            'Ansible for DevOps',
+            'Servers for hackers',
+        ]
+    ],
+    [
+        'name' => 'Barry',
+        'books' => [
+            'Working effectively with unit tests',
+            '50 quick ideas for your tests',
+        ],
+    ],
+    [
+        'name' => 'Jessica',
+        'books' => [
+            'Understanding the 4 rules of simple design',
+            'Principles of package design',
+        ],
+    ],
+    [
+        'name' => 'Clark',
+        'books' => [
+            'Selling test driven projects',
+        ],
+    ]
+];
+
+function searchByName($friends, $name) {
+    $filtered = [];
+    foreach ($friends as $friend) {
+        if (strpos($friends['name'], $name) == false) {
+            $filtered[] = $friend;
+        }
+    }
+}
+
+do {
+    echo '1. Show friends books', PHP_EOL;
+    echo '2. Search by name', PHP_EOL;
+    echo '3. Exit', PHP_EOL;
+    $option = fgets(STDIN);
+
+    switch ($option) {
+        case 1:
+            // TODO: Implement this one later...
+            break;
+        case 2:
+            //It's not returning an array with the names that are similar to $name
+            echo 'Please enter a name: ';
+            $name = fgets(STDIN);
+            var_dump(searchByName($name));
+            break;
+    }
+} while ($option != '3');
