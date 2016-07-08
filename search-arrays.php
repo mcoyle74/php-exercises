f695d8f8462802edde616c8e6c0f531c98caca22
Michael Coyle <mcoyle74@gmail.com>
2016-07-08 14:40:59 -0500

    Created a function that returns TRUE if an array value is found or FALSE otherwise. Searched for all the values in $names and as many that should return false to make sure. Works as expected.

--- search-arrays.php
+++ search-arrays.php
@@ -3,3 +3,22 @@
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+function arrayHasValue($array, $value) {
+	$result = array_search($value, $array);
+	if ($result === false) {
+		return false;
+	}
+	return true;
+}
+
+var_dump(arrayHasValue($names, 'Tina'));
+var_dump(arrayHasValue($names, 'Dana'));
+var_dump(arrayHasValue($names, 'Mike'));
+var_dump(arrayHasValue($names, 'Amy'));
+var_dump(arrayHasValue($names, 'Adam'));
+var_dump(arrayHasValue($names, 'Bob'));
+var_dump(arrayHasValue($names, 'Rob'));
+var_dump(arrayHasValue($names, 'Job'));
+var_dump(arrayHasValue($names, 'Cob'));
+var_dump(arrayHasValue($names, 'Blob'));
