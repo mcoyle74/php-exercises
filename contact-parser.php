b8e7b2b3f0e4f340cd15d52656cf8d86f066c535
Michael Coyle <mcoyle74@gmail.com>
2016-07-11 14:21:11 -0500

    Added a foreach() that formats contact numbers with dashes between appropiated digits.

--- contact-parser.php
+++ contact-parser.php
@@ -18,6 +18,13 @@ function parseContacts($filename)
 		$contacts[] = array_combine($keys, explode('|', $value));
 	}
 
+	foreach ($contacts as &$contact) {
+		$contact['number'] = substr_replace($contact['number'], '-', 3, 0);
+		$contact['number'] = substr_replace($contact['number'], '-', 7, 0);
+	}
+
+	fclose($handle);
+
 	return $contacts;
 }
 
