d01cd8deceed56adb742d8f98f77017e198b2f2b
Michael Coyle <mcoyle74@gmail.com>
2016-07-11 10:14:46 -0500

    Converted $physicistsString string to an array and var_dump'ed the result.

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,3 +1,8 @@
 <?php
 
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+$physicistsArray = explode(', ', $physicistsString);
+var_dump($physicistsArray);
+
+ // echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
