f658c87f76ee7b7c8d493ac104956c7c66a784ca
Michael Coyle <mcoyle74@gmail.com>
2016-07-11 10:47:23 -0500

    Popped the last item (Tony Stark) off $physicistsArray, saved it to a variable, used array_push() to add both 'and' and Stark onto end of array, and use implode() with comma to convert back to string. Used str_replace() to remove comma after 'and.'

--- list_with_commas.php
+++ list_with_commas.php
@@ -3,6 +3,13 @@
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
 $physicistsArray = explode(', ', $physicistsString);
-var_dump($physicistsArray);
 
- // echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
+$pop = array_pop($physicistsArray);
+
+array_push($physicistsArray, 'and', $pop);
+
+$famousFakePhysicists = implode(', ', $physicistsArray);
+
+$famousFakePhysicists = str_replace('and,', 'and', $famousFakePhysicists);
+
+ echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
