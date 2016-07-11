fbe9c14b1f81bdf09c7a57e3db58ba04c7568867
Michael Coyle <mcoyle74@gmail.com>
2016-07-11 16:12:16 -0500

    Set variable $filename to the log-YYYY-MM-DD.log format for the current date.

--- logger.php
+++ logger.php
@@ -2,7 +2,11 @@
 
 function logMessage($logLevel, $message)
 {
-    // todo - complete this function
+	$filename = 'log' . '-' . date('Y', time()) . '-' . date('m', time()) . '-' . date('d', time()) . '.log' . PHP_EOL;
+
+    $handle = fopen($filename, 'a');
+
+    fclose($handle);
 }
 
 logMessage("INFO", "This is an info message.");
