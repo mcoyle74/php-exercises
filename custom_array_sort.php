3586c4802a0316b71f6404065b3cb5f5e285d83b
Michael Coyle <mcoyle74@gmail.com>
2016-07-12 09:21:48 -0500

    Attempted to write a function that sorts the array alphabetically, but it just returns the array in the same order.

--- custom_array_sort.php
+++ custom_array_sort.php
@@ -2,6 +2,13 @@
 
 $whyIsTheAlphabetInThatOrder = ['e', 'a', 'g', 'c', 'i', 'd', 'f', 'b', 'h'];
 
-function sortArrayByAlphabet() {
-	
+function sortArrayByAlphabet(&$array) {
+	foreach ($array as $key => $letter) {
+		if ($letter[$key] < $letter[($key + 1)]) {
+			array_splice($array, $key, 0, ($key + 1));
+		}
+	}
+	return $array;
 }
+
+var_dump(sortArrayByAlphabet($whyIsTheAlphabetInThatOrder));
