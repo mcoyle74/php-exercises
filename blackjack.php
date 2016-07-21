289eee9f48f5a8d34f6fef59d19e7c707247aaba
Michael Coyle <mcoyle74@gmail.com>
2016-07-21 17:06:07 -0500

    Changed the name of $cards array to $ranks (just makes more sense to me as a card has both a rank and a suit). Completed the buildDeck() function by initializing $cards as am empty array and foreaching through both $ranks[] and $suits[], nested, and pushing a string with a rank for each suit onto the $cards array.

--- blackjack.php
+++ blackjack.php
@@ -6,19 +6,25 @@
 $suits = ['C', 'H', 'S', 'D'];
 
 // create an array for cards
-$cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+$ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
 // build a deck (array) of cards
 // card values should be "VALUE SUIT". ex: "7 H"
 // make sure to shuffle the deck before returning it
-function buildDeck($suits, $cards) {
-  // todo
+function buildDeck($suits, $ranks) {
+	$cards = [];
+	foreach ($ranks as $rank) {
+ 		foreach ($suits as $suit) {
+ 			$cards[] = "{$rank} {$suit}";
+ 		}
+	}
+	var_dump($cards);
 }
 
 // determine if a card is an ace
 // return true for ace, false for anything else
 function cardIsAce($card) {
-  // todo
+	// todo
 }
 
 // determine the value of an individual card (string)
@@ -26,20 +32,20 @@ function cardIsAce($card) {
 // face cards are worth 10
 // numeric cards are worth their value
 function getCardValue($card) {
-  // todo
+	// todo
 }
 
 // get total value for a hand of cards
 // don't forget to factor in aces
 // aces can be 1 or 11 (make them 1 if total value is over 21)
 function getHandTotal($hand) {
-  // todo
+	// todo
 }
 
 // draw a card from the deck into a hand
 // pass by reference (both hand and deck passed in are modified)
 function drawCard(&$hand, &$deck) {
-  // todo
+	// todo
 }
 
 // print out a hand of cards
@@ -50,11 +56,11 @@ function drawCard(&$hand, &$deck) {
 // or:
 // Player: [J D] [2 D] Total: 12
 function echoHand($hand, $name, $hidden = false) {
-  // todo
+	// todo
 }
 
 // build the deck of cards
-$deck = buildDeck($suits, $cards);
+$deck = buildDeck($suits, $ranks);
 
 // initialize a dealer and player hand
 $dealer = [];
@@ -70,8 +76,8 @@ $player = [];
 // todo
 
 // allow player to "(H)it or (S)tay?" till they bust (exceed 21) or stay
-while (/* todo */) {
-  // todo
+while (false) {
+	// todo
 }
 
 // show the dealer's hand (all cards)
