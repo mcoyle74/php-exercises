b7f3500d8112b41ad30ff07a26a7ae63043d5b49
Michael Coyle <mcoyle74@gmail.com>
2016-07-21 14:29:18 -0500

    Converted the functions in logger.php to a class.

--- Log.php
+++ Log.php
@@ -4,17 +4,18 @@ class Log
 {
 	public $filename;
 	public function logMessage($logLevel, $message) {
-		$filename = 'log-' . date('Y-m-d') . '.log';
+		// $filename = 'log-' . date('Y-m-d') . '.log';
+		$filename = $this->filename;
 		$handle = fopen($filename, 'a');
 		$entry = date('Y-m-d H:i:s') . " [$logLevel]" . " $message" . PHP_EOL;
 		fwrite($handle, $entry);
 		fclose($handle);
 	}
 	public function info() {
-
+		logMessage('INFO', $message);
 	}
 	public function error() {
-
+		logMessage('ERROR', $message);
 	}
 
 }
