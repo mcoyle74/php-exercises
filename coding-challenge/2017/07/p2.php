0fa94010e112278d26a291673a1d4d4b91fe2540
Michael Coyle <mcoyle74@gmail.com>
2017-07-27 20:14:21 -0500

    Fixed function so it returns first pair of integers within the array whose sum equals the value, rather than the first pair found by the for loops with the given sum.

--- coding-challenge/2017/07/p2.php
+++ coding-challenge/2017/07/p2.php
@@ -8,17 +8,22 @@
 	output: [4, 2]					output: [3, 7]
 */
 
-function first2thatSum($list, $n) {
+function first2IntegersEqualsValue($list, $n) {
 
 	$list = explode(', ', $list);
+	$keys = [];
 
 	for ($i = 0; $i < count($list); $i++) {
 		for ($j = $i + 1; $j < count($list); $j++) {
 			if ($list[$i] + $list[$j] == $n) {
-				return $list[$i] . ', ' . $list[$j];
+				$keys[] = $j;
 			}
 		}
 	}
+
+	$addend = $list[min($keys)];
+
+	return ($n - $addend) . ', ' . $addend;
 
 }
 
@@ -30,6 +35,6 @@ do {
 	fwrite(STDOUT, 'Enter number: ');
 	$n = trim(fgets(STDIN));
 	
-	echo first2thatSum($list, $n), PHP_EOL;
+	echo first2IntegersEqualsValue($list, $n), PHP_EOL;
 	
 } while ($list != 'q');
